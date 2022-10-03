08730939797994d476b7a0e4273b78868f7cb7a0
AmdouniBrahmiKhouloud <khouloud.amdounibrahmi@esprit.tn>
2022-10-03 02:05:57 +0100

    migrations , modeles and foctories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Ingredient;
+use App\Models\Recipe;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->has(
+             Recipe::factory(3)->hasAttached(
+                 Ingredient::factory(5),
+                 [
+                     'amount' => 10,
+                     'unit' => 'cl'
+                 ]
+             )
+         )
+
+             ->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
